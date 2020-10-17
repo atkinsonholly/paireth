ed88c6fd0a329494bbbf62e0c869cdedd319dda0
atkinsonholly <atkinsonholly@gmail.com>
2020-10-17 14:07:44 +0100

    outline paireth create-eth-app

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,25 +1,33 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Contract } from "@ethersproject/contracts";
-import { Web3Provider, getDefaultProvider } from "@ethersproject/providers";
+// import { Contract } from "@ethersproject/contracts";
+import { Web3Provider } from "@ethersproject/providers";
 import { useQuery } from "@apollo/react-hooks";
 
-import { Body, Button, Header, Image, Link } from "./components";
-import { web3Modal, logoutOfWeb3Modal } from './utils/web3Modal'
-import logo from "./ethereumLogo.png";
+import { Body, Button, Header, Image, HeroImage, Link, LogoSection, TokenInput, Tokens, Label, Subtitle, PairAddress, About } from "./components";
+import { web3Modal, logoutOfWeb3Modal } from "./utils/web3Modal";
+import logo from "./Paireth_large.png";
+import uniswapLogo from "./uniswap.svg";
 
-import { addresses, abis } from "@project/contracts";
+// import { addresses, abis } from "@project/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
-}
+import { checkForPair, createPair } from "./utils/generatePair";
+
+const SAND_ADDRESS_MAINNET = "0x3845badade8e6dff049820680d1f14bd3903a5d0";
+const ETH_ADDRESS_MAINNET = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+const PAIR_ADDRESS = "0x3dd49f67e9d5bc4c5e6634b3f70bfd9dc1b6bd74";
+
+// async function readOnChainData() {
+//   // Should replace with the end-user wallet, e.g. Metamask
+//   const defaultProvider = getDefaultProvider();
+
+//   // Create an instance of an ethers.js Contract
+//   // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
+//   const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
+//   // A pre-defined address that owns some CEAERC20 tokens
+//   const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
+//   console.log({ tokenBalance: tokenBalance.toString() });
+// }
 
 function WalletButton({ provider, loadWeb3Modal }) {
   return (
@@ -38,8 +46,49 @@ function WalletButton({ provider, loadWeb3Modal }) {
 }
 
 function App() {
-  const { loading, error, data } = useQuery(GET_TRANSFERS);
+  const { loading, error, data } = useQuery(GET_TRANSFERS); // update for subgraph
   const [provider, setProvider] = useState();
+  const [token0, setToken0] = useState("");
+  const [token1, setToken1] = useState("");
+  const [returnedPairAddress, setReturnedPairAddress] = useState(null);
+
+  const submitTokensForChecking = async(provider, token0, token1) => {
+    let pairAddress;
+    if (provider) {
+      pairAddress = await checkForPair(provider.getSigner(), token0, token1);
+    }
+    console.log(pairAddress)
+    setReturnedPairAddress(pairAddress);
+    return pairAddress;
+  }
+
+  const submitTokensForPairCreation = async(provider, token0, token1) => {
+    let pairAddress;
+    if (provider) {
+      pairAddress = await createPair(provider.getSigner(), token0, token1); 
+    }
+    console.log(pairAddress)
+    setReturnedPairAddress(pairAddress)
+    return pairAddress;
+  }
+
+  const checkReturnedPairAddress = () => {
+    if (returnedPairAddress.pair === "0x0000000000000000000000000000000000000000") {
+      return (
+        <>
+          <div>Pair does not exist on selected network. Would you like to create this pair?</div>
+          <Button onClick={() => submitTokensForPairCreation(provider, token0, token1)}>
+            Create Pair!
+          </Button>
+        </>
+      )
+    }
+    else {
+      return (
+        <>Pair Address: {returnedPairAddress}</>
+      )
+    }
+  }
 
   /* Open wallet selection modal. */
   const loadWeb3Modal = useCallback(async () => {
@@ -56,34 +105,41 @@ function App() {
 
   React.useEffect(() => {
     if (!loading && !error && data && data.transfers) {
-      console.log({ transfers: data.transfers });
+      console.log({ transfers: data.transfers }); // update for subgraph
     }
   }, [loading, error, data]);
 
   return (
-    <div>
+    <>
       <Header>
+        <LogoSection>
+          <Image src={uniswapLogo} alt="react-logo" />
+          <Link
+            href="https://info.uniswap.org/pairs"
+            style={{ marginTop: "8px" }}
+          >
+            Uniswap Pairs
+          </Link>
+        </LogoSection>
         <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} />
       </Header>
       <Body>
-        <Image src={logo} alt="react-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button hidden onClick={() => readOnChainData()}>
-          Read On-Chain Balance
+        <HeroImage src={logo} alt="react-logo" />
+        <Subtitle>Enter Token Addresses</Subtitle>
+        <About>Search Uniswap token pairs. Create new ones.</About>
+        <Tokens>
+          <Label>Token0</Label>
+          <TokenInput placeholder="Token address" onChange={e => setToken0(e.target.value)} value={token0}></TokenInput>
+          <Label>Token1</Label>
+          <TokenInput placeholder="Token address" onChange={e => setToken1(e.target.value)} value={token1}></TokenInput>
+        </Tokens>
+        
+        <Button onClick={() => submitTokensForChecking(provider, token0, token1)}>
+          Find Pair!
         </Button>
-        <Link
-          href="https://ethereum.org/developers/#getting-started"
-          style={{ marginTop: "8px" }}
-        >
-          Learn Ethereum
-        </Link>
-        <Link href="https://reactjs.org">Learn React</Link>
-        <Link href="https://thegraph.com/docs/quick-start" >Learn The Graph</Link>
+        <PairAddress>{returnedPairAddress ? checkReturnedPairAddress(): null}</PairAddress>
       </Body>
-    </div>
+    </>
   );
 }
 
--- packages/react-app/src/components/index.js
+++ packages/react-app/src/components/index.js
@@ -1,18 +1,28 @@
 import styled from "styled-components";
+const backgroundImage = '/background.jpg';
 
 export const Header = styled.header`
-  background-color: #282c34;
+  background-color: #4752ff;
   min-height: 70px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   color: white;
+`;
+
+export const LogoSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Body = styled.body`
   align-items: center;
-  background-color: #282c34;
+  background-image: url(${process.env.PUBLIC_URL + backgroundImage});
+  background-repeat:no-repeat;
+  background-size:cover;
   color: white;
   display: flex;
   flex-direction: column;
@@ -22,30 +32,89 @@ export const Body = styled.body`
 `;
 
 export const Image = styled.img`
+  height: 10vmin;
+  pointer-events: none;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+export const HeroImage = styled.img`
   height: 40vmin;
   margin-bottom: 16px;
   pointer-events: none;
+`;
+
+export const TokenInput = styled.input`
+  box-sizing: border-box,
+  width: 300px,
+  height: 75px,
+  border-radius: 6px,
+  border: none,
+  outline: none,
+  font-size: 16px,
+  padding: 10px,
+  overflow: scroll,
+  resize: none,
+`;
+
+export const Subtitle = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const About = styled.div`
+  font-size: 16px;
+  margin-bottom: 20px;
+`;
+
+export const PairAddress = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  color: #4752ff;
+  min-height: 80px;
+`;
+
+export const Label = styled.div`
+  font-size: 16px;
+  margin-left: 20px;
+  margin-right: 5px;
+`;
+
+export const Tokens = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
 `;
 
 export const Link = styled.a.attrs({
   target: "_blank",
   rel: "noopener noreferrer",
 })`
-  color: #61dafb;
-  margin-top: 10px;
+  color: #ffffff;
+  font-size: 16px;
+  text-decoration: none;
 `;
 
 export const Button = styled.button`
-  background-color: white;
-  border: none;
+  background-color: #4752ff;
+  border: 2px solid #ffffff;
   border-radius: 8px;
-  color: #282c34;
+  color: #ffffff;
   cursor: pointer;
   font-size: 16px;
   text-align: center;
   text-decoration: none;
   margin: 0px 20px;
   padding: 12px 24px;
+  min-width: 180px;
+  font-weight: 700;
+
 
   ${props => props.hidden && "hidden"}
   
